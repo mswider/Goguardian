bdf0989cff34676cc306239ae46b813748811e43
Matthew Swider <matthew.swider@outlook.com>
2020-10-29 17:06:17 -0500

    Created initial test to be added onto later

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello from Express!');
+});
+
+var port = 3000;
+
+app.listen(port, function () {
+  console.log(`Express started on port ${port}`);
+});
