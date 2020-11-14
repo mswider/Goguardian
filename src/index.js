b90e747ed39ac24c4fa33592f60c7766a33596ec
mswider <matthew.swider@outlook.com>
2020-11-13 18:03:25 -0600

    Completed IP address control and added web interface

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const fs = require('fs')
 const app = express();
+app.use(bodyParser.text())
+
+var ipSettings = { spoofEnabled: false, customIP: '8.8.8.8' };
 
 app.get('/ip', function (req, res) {
-  res.send('8.8.8.8');
+  if(ipSettings.spoofEnabled){
+    res.send(ipSettings.customIP);
+  } else {
+    (async () => {
+	   const response = await fetch('https://ip.goguardian.com/ip');
+	   const body = await response.text();
+	   res.send(body);
+    })();
+  }
+});
+
+app.get('/', function (req, res) {
+  res.redirect('/web');
+});
+
+app.get('/web', function (req, res) {
+  fs.readFile('src/index.html', 'utf8' , (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.get('/web/api/ip', function (req, res) {
+  var data = ipSettings;
+  (async () => {
+	 const response = await fetch('https://ip.goguardian.com/ip');
+	 const body = await response.text();
+	 data.realIP = body;
+   res.send(data);
+  })();
+});
+app.post('/web/api/set_ip', function (req, res, next) {
+  var data = JSON.parse(req.body);
+  ipSettings.spoofEnabled = data.spoofEnabled;
+  ipSettings.customIP = data.customIP;
+  res.sendStatus(200);
 });
 
 var port = 3000;
